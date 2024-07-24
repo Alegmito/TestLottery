d7c1efcbff2dedbd3517a20a483bbf489109e85f
Daniil Smirnov <daniil.smirnov007@gmail.com>
2024-07-24 13:54:36 +0300

    feat: added tests and alternative implementation

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TestLottery.LotteryTest;
 
-Console.WriteLine("Hello, World!");
-var testLotteries = new List<NaiveLotterySolution>();
-var NumberOfSteps = 5;
-for (int i = 2; i <= NumberOfSteps + 2; i ++)
-{
-  testLotteries.Add(new NaiveLotterySolution(i));
-}
+Console.WriteLine("Enter Two number, separated by comma: the number of lottery participants, kick-out step");
+var lotteryArgs = Console.ReadLine()?.Split(',').Select((string str) => 
+  {
+    int result = 0;
+    int.TryParse(str, out result);
+    return result;
+  }).Take(2).ToList();
 
-var participantsCounts = Enumerable.Range(1, 20);
-
-foreach (var testLottery in testLotteries)
-{
-  Console.WriteLine(
-  testLottery.MakeSeveralLotteries(participantsCounts.ToList()));
-}
-
+var lottery = new Lottery(lotteryArgs[1]);
+Console.WriteLine($"The winner is: {lottery.GetLotteryWinnerAlt(lotteryArgs[0])}");
 Console.ReadLine();
